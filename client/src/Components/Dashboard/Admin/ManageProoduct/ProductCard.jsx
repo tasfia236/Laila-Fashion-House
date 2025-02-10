60566c78383811cf31e4effe661b92f2256fd919
tasfia236 <tasfiatabassum300@gmail.com>
2025-02-10 20:26:57 +0600

    modify color theme

--- client/src/Components/Dashboard/Admin/ManageProoduct/ProductCard.jsx
+++ client/src/Components/Dashboard/Admin/ManageProoduct/ProductCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function ProductCard ({ product, handleDeleteProduct }) {
   return (
-    <div className='relative flex flex-col flex-grow bg-center shadow-lg rounded-lg w-80 h-full transform transition-transform duration-300 overflow-hidden'>
+    <div className='relative flex flex-col flex-grow bg-center shadow-lg rounded-lg w-80 h-full overflow-hidden transition-transform duration-300 transform'>
       {/* Product Image */}
       <img
         src={product?.productImage}
@@ -12,33 +12,33 @@ export default function ProductCard ({ product, handleDeleteProduct }) {
       />
 
       {/* Overlay */}
-      <div className='z-20 absolute inset-0 bg-purple800 bg-opacity-70'></div>
+      <div className='z-20 absolute inset-0 bg-purple-900 bg-opacity-50'></div>
 
-      <div className='relative z-50 flex flex-col flex-grow p-4 sm:p-6 text-white'>
-        <div className='top-2 right-2 absolute flex-grow bg-[#be185d] bg-yellow-400 m-2 px-6 p-2 rounded-full font-bold text-white text-xs'>
+      <div className='z-50 relative flex flex-col flex-grow p-4 sm:p-6 text-white'>
+        <div className='top-2 right-2 absolute flex-grow bg-[#be185d] m-2 p-2 px-6 rounded-full font-bold text-white text-xs'>
           {product?.categoryName}
         </div>
 
         {/* Product Details */}
         <div className='p-5'>
           {/* Product Name */}
-          <h2 className='flex-grow pt-6 font-bold text-gray-800 text-lg truncate'>
+          <h2 className='flex-grow pt-6 font-bold text-gray-100 text-lg truncate'>
             {product?.productName}
           </h2>
 
           {/* Product Description */}
-          <p className='flex-grow flex-shrink mt-2 line-clamp-3 h-20 text-gray-600 text-sm'>
+          <p className='flex-grow flex-shrink mt-2 h-20 text-gray-300 text-sm line-clamp-3'>
             {product?.description}
           </p>
 
           {/* Fabric */}
-          <div className='mt-3 text-gray-500 text-sm'>
+          <div className='mt-3 text-gray-200 text-sm'>
             <span className='font-semibold'>Fabric:</span> {product?.fabric}
           </div>
           {/* Sizes and Price */}
           <div className='flex justify-between items-center mt-4'>
             {/* Sizes */}
-            <div className='mt-1 text-gray-500 text-sm'>
+            <div className='mt-1 text-gray-200 text-sm'>
               <span className='font-semibold'>Size:</span>{' '}
               {(product?.size || []).join(', ') || 'N/A'}
             </div>
@@ -52,13 +52,13 @@ export default function ProductCard ({ product, handleDeleteProduct }) {
           {/* Add to Cart and Details Button */}
           <div className='flex justify-between mt-6'>
             <Link to={`dashboard/updateproduct/${product._id}`}>
-              <button className='border-2 border-white bg-transparent hover:bg-[#be185d] shadow-lg px-4 py-2 rounded-md font-semibold text-sm text-white transition duration-200'>
+              <button className='bg-transparent hover:bg-[#be185d] shadow-lg px-4 py-2 border-2 border-white rounded-md font-semibold text-white text-sm transition duration-200'>
                 Update
               </button>
             </Link>
             <button
               onClick={() => handleDeleteProduct(product)}
-              className='border-2 border-white bg-[#be185d] hover:bg-transparent px-4 py-2 rounded-md font-semibold text-sm text-white transition duration-200 outline-none'
+              className='bg-[#be185d] hover:bg-transparent px-4 py-2 border-2 border-white rounded-md outline-none font-semibold text-white text-sm transition duration-200'
             >
               Delete
             </button>
